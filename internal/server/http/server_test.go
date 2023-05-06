3f386bb689760cd563b21bda79b33b645945e6c7
skolzkyi <skolzkyi@gmail.com>
2023-05-06 17:48:56 +0400

    WIP server test

--- internal/server/http/server_test.go
+++ internal/server/http/server_test.go
@@ -6,20 +6,21 @@ package internalhttp
 import (
 	"bytes"
 	"context"
-	"errors"
-	"fmt"
+	//"errors"
+	//"fmt"
 	"io"
 	"net/http/httptest"
-	"os"
-	"strconv"
+	//"os"
+	//"strconv"
 	"testing"
 	"time"
-	"encoding/json"
+	//"encoding/json"
 
-	"github.com/skolzkyi/antibruteforce/internal/app"
-	"github.com/skolzkyi/antibruteforce/internal/logger"
+	app "github.com/skolzkyi/antibruteforce/internal/app"
 	storageData  "github.com/skolzkyi/antibruteforce/internal/storage/storageData"
-	//memorystorage "github.com/skolzkyi/hwOTUS_YIA/hw12_13_14_15_calendar/internal/storage/memory"
+	storageSQLMock "github.com/skolzkyi/antibruteforce/internal/storage/storageSQLMock"
+	RedisStorage "github.com/skolzkyi/antibruteforce/internal/storage/redis"
+	logger "github.com/skolzkyi/antibruteforce/internal/logger"
 	"github.com/stretchr/testify/require"
 )
 
@@ -41,20 +42,12 @@ func (config *ConfigTest) GetPort() string {
 	return "4000"
 }
 
-func (config *ConfigTest) GetGRPCPort() string {
-	return "5000"
-}
-
-func (config *ConfigTest) GetOSFilePathSeparator() string {
-	return string(os.PathSeparator)
-}
-
 func (config *ConfigTest) GetServerShutdownTimeout() time.Duration {
 	return 5 * time.Second
 }
 
 func (config *ConfigTest) GetDBName() string {
-	return "OTUSFinalLab"
+	return "OTUSAntibruteforce"
 }
 
 func (config *ConfigTest) GetDBUser() string {
@@ -89,23 +82,306 @@ func (config *ConfigTest) GetDBPort() string {
 	return "3306"
 }
 
-func TestCreateEvent(t *testing.T) {
-	data := bytes.NewBufferString(getTestEventData(t))
-	server := createServer(t)
-
-	r := httptest.NewRequest("POST", "/Event/", data)
-	w := httptest.NewRecorder()
-	server.Event_REST(w, r)
-
-	res := w.Result()
-	defer res.Body.Close()
-	respBody, err := io.ReadAll(res.Body)
-	require.NoError(t, err)
-
-	respExp := `{"Text":"OK!","Code":0}`
-	require.Equal(t, respExp, string(respBody))
+func (config *ConfigTest) GetResisAddress() string {
+	return "127.0.0.1"
 }
 
+func (config *ConfigTest) GetRedisPort() string {
+	return "6379"
+}
+
+func (config *ConfigTest) GetLimitFactorLogin() int{
+	return 10
+}
+
+func (config *ConfigTest) GetLimitFactorPassword() int {
+	return 100
+}
+
+func (config *ConfigTest) GetLimitFactorIP() int{
+	return 12
+}
+
+func (config *ConfigTest) GetLimitTimeCheck() time.Duration {
+	return 60 * time.Second
+}
+
+func TestWhiteListREST(t *testing.T) {
+	t.Run("AddIPToWhiteList", func(t *testing.T) {
+		t.Parallel()
+		data := bytes.NewBufferString(`{
+			"IP":"192.168.16.0",
+			"Mask":8
+		
+		}`)
+		server := createServer(t)
+		
+		r := httptest.NewRequest("POST", "/whitelist/", data)
+		w := httptest.NewRecorder()
+		server.WhiteList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		respExp := `{"Text":"OK!","Code":0}`
+		require.Equal(t, respExp, string(respBody))
+	})
+	t.Run("IsIPInWhiteList", func(t *testing.T) {
+		t.Parallel()
+		newData:=storageData.StorageIPData{
+			IP:   "192.168.16.0",
+			Mask: 8,
+		}
+		
+		data := bytes.NewBufferString(`{
+			"IP":"192.168.16.0",
+			"Mask":8
+		
+		}`)
+		server := createServer(t)
+
+		_, err := server.app.AddIPToWhiteList(context.Background(), newData)
+		require.NoError(t, err) 
+		
+		r := httptest.NewRequest("GET", "/whitelist/", data)
+		w := httptest.NewRecorder()
+		server.WhiteList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		respExp := `{"IPList":[],"Message":{"Text":"YES","Code":0}}`
+		require.Equal(t, respExp, string(respBody))
+	})
+	t.Run("RemoveIPInWhiteList", func(t *testing.T) {
+		t.Parallel()
+		newData:=storageData.StorageIPData{
+			IP:   "192.168.16.0",
+			Mask: 8,
+		}
+		
+		data := bytes.NewBufferString(`{
+			"IP":"192.168.16.0",
+			"Mask":8
+		
+		}`)
+		server := createServer(t)
+
+		_, err := server.app.AddIPToWhiteList(context.Background(), newData)
+		require.NoError(t, err) 
+		controlDataSl,err:=server.app.GetAllIPInWhiteList(context.Background())
+		require.NoError(t, err) 
+		flag:=false
+		for _,curControlData:=range controlDataSl {
+			if curControlData.IP =="192.168.16.0" && curControlData.Mask==8 {
+				flag=true
+				break
+			}
+		}
+		require.Equal(t, flag, true)
+
+		r := httptest.NewRequest("DELETE", "/whitelist/", data)
+		w := httptest.NewRecorder()
+		server.WhiteList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		respExp := `{"Text":"OK!","Code":0}`
+		require.Equal(t, respExp, string(respBody))
+
+		controlDataSl,err=server.app.GetAllIPInWhiteList(context.Background())
+		require.NoError(t, err) 
+		flag=false
+		for _,curControlData:=range controlDataSl {
+			if curControlData.IP =="192.168.16.0" && curControlData.Mask==8 {
+				flag=true
+				break
+			}
+		}
+		require.Equal(t, flag, false)
+	})
+	t.Run("GetAllIPInWhiteList", func(t *testing.T) {
+		t.Parallel()
+		data := bytes.NewBufferString(`{
+			"IP":"ALL",
+			"Mask":0
+		
+		}`)
+		newData:=storageData.StorageIPData{
+			IP:   "192.168.16.0",
+			Mask: 8,
+		}
+		server := createServer(t)
+
+		
+		_, err := server.app.AddIPToWhiteList(context.Background(), newData)
+		require.NoError(t, err) 
+		newData=storageData.StorageIPData{
+			IP:   "172.92.24.0",
+			Mask: 24,
+		}
+		_, err = server.app.AddIPToWhiteList(context.Background(), newData)
+		require.NoError(t, err) 
+
+		r := httptest.NewRequest("GET", "/whitelist/", data)
+		w := httptest.NewRecorder()
+		server.WhiteList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err) 
+
+		respExp := `{"IPList":[{"IP":"192.168.16.0","Mask":8,"ID":0},{"IP":"172.92.24.0","Mask":24,"ID":1}],"Message":{"Text":"OK!","Code":0}}`
+		require.Equal(t, respExp, string(respBody))
+	})
+}
+
+func TestBlackListREST(t *testing.T) {
+	t.Run("AddIPToBlackList", func(t *testing.T) {
+		t.Parallel()
+		data := bytes.NewBufferString(`{
+			"IP":"192.168.16.0",
+			"Mask":8
+		
+		}`)
+		server := createServer(t)
+		
+		r := httptest.NewRequest("POST", "/blacklist/", data)
+		w := httptest.NewRecorder()
+		server.BlackList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		respExp := `{"Text":"OK!","Code":0}`
+		require.Equal(t, respExp, string(respBody))
+	})
+	t.Run("IsIPInBlackList", func(t *testing.T) {
+		t.Parallel()
+		newData:=storageData.StorageIPData{
+			IP:   "192.168.16.0",
+			Mask: 8,
+		}
+		
+		data := bytes.NewBufferString(`{
+			"IP":"192.168.16.0",
+			"Mask":8
+		
+		}`)
+		server := createServer(t)
+
+		_, err := server.app.AddIPToBlackList(context.Background(), newData)
+		require.NoError(t, err) 
+		
+		r := httptest.NewRequest("GET", "/blacklist/", data)
+		w := httptest.NewRecorder()
+		server.BlackList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		respExp := `{"IPList":[],"Message":{"Text":"YES","Code":0}}`
+		require.Equal(t, respExp, string(respBody))
+	})
+	t.Run("RemoveIPInBlackList", func(t *testing.T) {
+		t.Parallel()
+		newData:=storageData.StorageIPData{
+			IP:   "192.168.16.0",
+			Mask: 8,
+		}
+		
+		data := bytes.NewBufferString(`{
+			"IP":"192.168.16.0",
+			"Mask":8
+		
+		}`)
+		server := createServer(t)
+
+		_, err := server.app.AddIPToBlackList(context.Background(), newData)
+		require.NoError(t, err) 
+		controlDataSl,err:=server.app.GetAllIPInBlackList(context.Background())
+		require.NoError(t, err) 
+		flag:=false
+		for _,curControlData:=range controlDataSl {
+			if curControlData.IP =="192.168.16.0" && curControlData.Mask==8 {
+				flag=true
+				break
+			}
+		}
+		require.Equal(t, flag, true)
+
+		r := httptest.NewRequest("DELETE", "/blacklist/", data)
+		w := httptest.NewRecorder()
+		server.BlackList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		respExp := `{"Text":"OK!","Code":0}`
+		require.Equal(t, respExp, string(respBody))
+
+		controlDataSl,err=server.app.GetAllIPInBlackList(context.Background())
+		require.NoError(t, err) 
+		flag=false
+		for _,curControlData:=range controlDataSl {
+			if curControlData.IP =="192.168.16.0" && curControlData.Mask==8 {
+				flag=true
+				break
+			}
+		}
+		require.Equal(t, flag, false)
+	})
+	t.Run("GetAllIPInBlackList", func(t *testing.T) {
+		t.Parallel()
+		data := bytes.NewBufferString(`{
+			"IP":"ALL",
+			"Mask":0
+		
+		}`)
+		newData:=storageData.StorageIPData{
+			IP:   "192.168.16.0",
+			Mask: 8,
+		}
+		server := createServer(t)
+
+		
+		_, err := server.app.AddIPToBlackList(context.Background(), newData)
+		require.NoError(t, err) 
+		newData=storageData.StorageIPData{
+			IP:   "172.92.24.0",
+			Mask: 24,
+		}
+		_, err = server.app.AddIPToBlackList(context.Background(), newData)
+		require.NoError(t, err) 
+
+		r := httptest.NewRequest("GET", "/blacklist/", data)
+		w := httptest.NewRecorder()
+		server.BlackList_REST(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		respBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err) 
+
+		respExp := `{"IPList":[{"IP":"192.168.16.0","Mask":8,"ID":0},{"IP":"172.92.24.0","Mask":24,"ID":1}],"Message":{"Text":"OK!","Code":0}}`
+		require.Equal(t, respExp, string(respBody))
+	})
+}
+/*
 func TestCreateEventBadTimeBusy(t *testing.T) {
 	data := bytes.NewBufferString(getTestEventData(t))
 	server := createServer(t)
@@ -466,28 +742,23 @@ func getTestEventData(t *testing.T) string {
 		"EventMessageTimeDelta": 10800000
 	}`
 }
-
+*/
 func createServer(t *testing.T) *Server {
 	t.Helper()
-	config := ConfigTest{}
-
-	fmt.Println("config: ", config)
-	log, err := logger.New("debug")
+	t.Helper()
+	logger, _ := logger.New("debug")
+	config:=ConfigTest{}
+	var storage app.Storage
+	storage = storageSQLMock.New()
+	ctxStor, _ := context.WithTimeout(context.Background(), config.GetDBTimeOut())
+	err := storage.Init(ctxStor, logger, &config)
 	require.NoError(t, err)
-	if err != nil {
-		t.Fatal()
-	}
-
-	storage := memorystorage.New()
-	err = storage.Init(context.Background(), log, &config)
+	redis := RedisStorage.New()
+	err = redis.InitAsMock(ctxStor, logger)
 	require.NoError(t, err)
-	if err != nil {
-		t.Fatal()
-	}
+	antibruteforce := app.New(logger, storage, redis, &config)
 
-	calendar := app.New(log, storage)
-
-	server := NewServer(log, calendar, &config)
+	server := NewServer(logger, antibruteforce, &config)
 
 	return server
 }
